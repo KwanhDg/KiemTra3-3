49bb4aca75a83130a5dfa0a1ead8d13f80f610b3
KwanhDg <101886812+KwanhDg@users.noreply.github.com>
2025-03-03 09:30:35 +0700

    update

--- screens/ScanScreen.js
+++ screens/ScanScreen.js
@@ -9,7 +9,14 @@ const ScanScreen = () => {
         style={styles.fullScreenImage}
       />
       <View style={styles.scanOverlay}>
-        <View style={styles.scanLine} />
+        <View style={styles.cornerTopLeftHorizontal} />
+        <View style={styles.cornerTopLeftVertical} />
+        <View style={styles.cornerTopRightHorizontal} />
+        <View style={styles.cornerTopRightVertical} />
+        <View style={styles.cornerBottomLeftHorizontal} />
+        <View style={styles.cornerBottomLeftVertical} />
+        <View style={styles.cornerBottomRightHorizontal} />
+        <View style={styles.cornerBottomRightVertical} />
       </View>
       <View style={styles.productInfo}>
         <Image
@@ -39,25 +46,95 @@ const styles = StyleSheet.create({
   },
   scanOverlay: {
     position: 'absolute',
-    width: '80%',
-    height: 100,
+    top: 150,
+    left: 44,
+    width: 287.5,
+    height: 400,
+    borderRadius: 35,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  scanLine: { width: '100%', height: 2, backgroundColor: '#007bff' },
+  cornerTopLeftHorizontal: {
+    position: 'absolute',
+    top: -2,
+    left: -2,
+    width: 20,
+    height: 3,
+    backgroundColor: '#FFF',
+  },
+  cornerTopLeftVertical: {
+    position: 'absolute',
+    top: -2,
+    left: -2,
+    width: 3,
+    height: 20,
+    backgroundColor: '#FFF',
+  },
+  cornerTopRightHorizontal: {
+    position: 'absolute',
+    top: -2,
+    right: -2,
+    width: 20,
+    height: 3,
+    backgroundColor: '#FFF',
+  },
+  cornerTopRightVertical: {
+    position: 'absolute',
+    top: -2,
+    right: -2,
+    width: 3,
+    height: 20,
+    backgroundColor: '#FFF',
+  },
+  cornerBottomLeftHorizontal: {
+    position: 'absolute',
+    bottom: -2,
+    left: -2,
+    width: 20,
+    height: 3,
+    backgroundColor: '#FFF',
+  },
+  cornerBottomLeftVertical: {
+    position: 'absolute',
+    bottom: -2,
+    left: -2,
+    width: 3,
+    height: 20,
+    backgroundColor: '#FFF',
+  },
+  cornerBottomRightHorizontal: {
+    position: 'absolute',
+    bottom: -2,
+    right: -2,
+    width: 20,
+    height: 3,
+    backgroundColor: '#FFF',
+  },
+  cornerBottomRightVertical: {
+    position: 'absolute',
+    bottom: -2,
+    right: -2,
+    width: 3,
+    height: 20,
+    backgroundColor: '#FFF',
+  },
   productInfo: {
+    position: 'absolute',
+    bottom: 50,
+    left: 42,
+    width: 292,
+    height: 82,
     flexDirection: 'row',
     alignItems: 'center',
     backgroundColor: '#fff',
     padding: 10,
     borderRadius: 10,
-    marginTop: 20,
+    borderWidth: 2,
+    borderColor: '#FFFFFF',
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 4,
-    width: '90%',
-    justifyContent: 'space-between',
   },
   productIcon: { width: 40, height: 40, marginRight: 10 },
   productDetails: { flex: 1 },
